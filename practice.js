1addaec892307da5ac1ce4048d5f014e9c363bf3
md monirul islam <mdmonirulislam419395@gmail.com>
2024-07-14 12:03:30 +0600

    condition

---
+++ practice.js
@@ -0,0 +1,32 @@
+
+
+let number1 = 90;
+let number2 = 70;
+let number3 = 80;
+
+
+if(number1 > number2  &&  number1 > number3){
+
+    console.log('number 1 is bigger then 2 and 3')
+
+}else if(number2 > number1  &&  number2 > number3){
+
+    console.log('number 2 in bigger then 1 and 3')
+
+}else{
+    console.log('number3 in bigger then 1 and 2')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+console.log()
