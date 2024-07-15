8044c3eb7c199373462d1787919466ac08cc6adb
md monirul islam <mdmonirulislam419395@gmail.com>
2024-07-15 11:56:42 +0600

    looping object

--- practice.js
+++ practice.js
@@ -1,100 +1,30 @@
+const person = {
+    name:'messi',
+    age:37,
+    nationality:'argentina',
+    height:'5 fit 7 inch'
+}
+
+console.log(person)
 
 
+let objProperty = Object.keys(person);
 
-// let i = 1;
-// while(i <= 10){
-//     if(i === 5) {
-//     i++;
-//         continue;
-//     }
-//     console.log(i);
-//     i++;
-// }
+let objValue = Object.values(person)
 
+for (let i = 0; i < objProperty.length; i++) {
 
-let arr = ['apple','mango','banana']
-
-for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-
-    if (arr[i] === 'mango') {
-
-        continue;
-        // break;
-        
-    }
-
-    console.log(element)
+    console.log(objProperty[i] ,"====" ,objValue[i])
     
 }
 
+console.log("------------------")
 
 
+for(let val in person){
+    console.log(val, '='  ,person[val])
 
-for (let i = 1; i <= 10; i++) {
-    const element = (i);
-
-    if (i === 5) {
-        continue;
-        
-    }
-    console.log(element)
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let number = [10,23,90,9,9,]
-
-let j = 0;
-
-while (j < number.length) {
-
-    if (number[j] === 23) {
-     j++;
-        continue;    
-    }
-
-    console.log(j);
-    j++ 
-}
-
-// jm  while loop 
-
-let i = 1;
-while(i <= 10){
-    if(i === 5) {
-    i++;
-        continue;
-    }
-    console.log(i);
-    i++;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
