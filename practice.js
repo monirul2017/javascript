98608cac1d5b070484442da289eee78386dd307f
md monirul islam <mdmonirulislam419395@gmail.com>
2024-07-16 17:49:06 +0600

    function for multipule parameter oparation

--- practice.js
+++ practice.js
@@ -1,25 +1,45 @@
 
-let arr = ['moni','raju','bayezid','anower','moni','raju','bayezid','anower','moni','raju','bayezid','anower','moni','raju','bayezid','anower'];
+for (let i = 0; i <= 20; i++) {
+  const element =i;
 
-console.log(arr)
+  if (i % 3 === 0 && i % 5 === 0) {
 
-function deleteDuplicateItem(){
+    console.log('this is number is multiplie of 3 and 5')
+    
+  }else if(i % 5 === 0){
+    console.log('this is number is multiplie of 5')
 
-  let newArray = []
+  }else if(i % 3 === 0){
 
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    if (newArray.includes(element) === false) {
+    console.log('this is number is multiplie of 3')
 
-      newArray.push(element)
-      
-    }
+  }else{
+  console.log(element)
+
   }
-  return newArray;
+  
 }
 
 
-let output = deleteDuplicateItem()
+// function for multipule calculation
+
+function calculation(book,pen,noots){
+
+  let perbookprice = 200;
+  let perpenprice = 20;
+  let pernootsprice = 50;
+
+  let bookprice = perbookprice * book;
+  let penprice = perpenprice * pen;
+  let nootsprice = pernootsprice * noots
+
+  let output = `Book Price: ${bookprice}, penprice:${penprice}, nootsprice : ${nootsprice},`;
+
+  return output;
+
+}
+
+let output = calculation(4,2,2)
 
 console.log(output)
 
