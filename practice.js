640f34746fddcde70fe79246983403a12b6fbdc2
md monirul islam <mdmonirulislam419395@gmail.com>
2024-07-16 18:59:39 +0600

    highest value check object under array

--- practice.js
+++ practice.js
@@ -1,51 +1,71 @@
 
-for (let i = 0; i <= 20; i++) {
-  const element =i;
+let phones = [
+  {name:'apple',price:90000,color:'silver'},
+  {name:'nokia',price:25000,color:'silver'},
+  {name:'oppo',price:18000,color:'black'},
+  {name:'hawei',price:30000,color:'black'},
+  {name:'google',price:65000,color:'silver'},
+  {name:'one plus',price:48000,color:'black'},
+]
 
-  if (i % 3 === 0 && i % 5 === 0) {
+console.log(phones)
 
-    console.log('this is number is multiplie of 3 and 5')
+
+function myfunction(){
+
+    let lowestPrice = phones[0].price;
+
+    for (let i = 0; i < phones.length; i++) {
+        const element = phones[i];
+
+        console.log(element)
+
+          if (lowestPrice > element.price) {
+              lowestPrice = element.price
+          }   
+    }
+    return lowestPrice;
+}
+
+
+let output = myfunction()
+
+console.log("---------------")
+console.log('lowest price is ', output)
+
+// ////////////////////////////
+console.log('==============')
+
+
+
+
+let products = [
+
+  {name:'phone',price:20000,color:'silver'},
+  {name:'book',price:500,},
+  {name:'pen',price:20},
+]
+
+
+function myfunction2(){
+
+  let totalPrice = 0;
+  for (let i = 0; i < products.length; i++) {
+    const element = products[i];
+    console.log(element)
+    totalPrice += element.price;
     
-  }else if(i % 5 === 0){
-    console.log('this is number is multiplie of 5')
-
-  }else if(i % 3 === 0){
-
-    console.log('this is number is multiplie of 3')
-
-  }else{
-  console.log(element)
-
   }
-  
+
+  return totalPrice;
 }
 
-
-// function for multipule calculation
-
-function calculation(book,pen,noots){
-
-  let perbookprice = 200;
-  let perpenprice = 20;
-  let pernootsprice = 50;
-
-  let bookprice = perbookprice * book;
-  let penprice = perpenprice * pen;
-  let nootsprice = pernootsprice * noots
-
-  let output = `Book Price: ${bookprice}, penprice:${penprice}, nootsprice : ${nootsprice},`;
-
-  return output;
-
-}
-
-let output = calculation(4,2,2)
-
-console.log(output)
+let totalPrice = myfunction2()
+console.log('total price is ',totalPrice)
 
 
 
-
+// ========================
 
 
 
